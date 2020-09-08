e36ac6ea5b201380be4e39d3b8691981c467370c
szatkoaleksander <szatko.aleksander@gmail.com>
2020-09-08 21:34:58 +0200

    Add Login and Register

---
+++ src/app/modules/auth/login/login.component.ts
@@ -0,0 +1,79 @@
+import { ModalService } from './../../../core/services/modal.service';
+import { Router } from '@angular/router';
+import { AuthService } from './../../../core/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import User from 'src/app/core/models/User';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  private modalSubscription: Subscription;
+
+  public loginForm: FormGroup;
+  public submitted: boolean = false;
+  public isLoading: boolean = false;
+  public apiError: string;
+
+  constructor(
+    private authService: AuthService,
+    private modalService: ModalService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.modalSubscription = this.modalService
+      .getModalState()
+      .subscribe((response) => {
+        if (!response.isOpen) {
+          this.onCloseModal();
+        }
+      });
+
+    this.loginForm = this.formBuilder.group({
+      email: ['a.szatko71@gmail.com', Validators.required],
+      password: ['ZAQ!2wsx', Validators.required],
+    });
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.modalSubscription.unsubscribe();
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService
+      .login(new User(this.f.email.value, this.f.password.value))
+      .subscribe(
+        () => {
+          this.modalService.close('login-modal');
+        },
+        ({ error }) => {
+          this.apiError = error.error.Message;
+        }
+      )
+      .add(() => {
+        this.isLoading = false;
+      });
+  }
+
+  onCloseModal(): void {
+    this.loginForm.reset();
+    this.apiError = '';
+    this.submitted = false;
+  }
+}
--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -3,9 +3,21 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlideshowModule } from 'ng-simple-slideshow';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [SlideshowModule, SharedModule, CommonModule],
+  declarations: [HomeComponent, LoginComponent, RegisterComponent],
+  imports: [
+    SlideshowModule,
+    SharedModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TranslateModule,
+  ],
+  exports: [TranslateModule],
 })
 export class ModulesModule {}
